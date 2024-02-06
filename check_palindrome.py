b03125c88f46f6fca0ec890c5b75c616c66ebe97
Katie Bridson <kbridson@memphis.edu>
2024-02-05 18:43:18 -0600

    Added choice of text input or filepath input and read file content

--- check_palindrome.py
+++ check_palindrome.py
@@ -4,6 +4,17 @@ def check_palindrome(text):
     else:
         print(f"{text} is not a palindrome")
 
+choice = input("Enter 't' for text or 'f' for file: ")
 
-text = input("Enter text to check for palindrome: ")
-check_palindrome(text)
+if choice == 't':
+    text = input("Enter text to check for palindrome: ")
+    check_palindrome(text)
+elif choice == 'f':
+    try:
+        filepath = input("Enter filepath for palindrome check: ")
+        text = open(filepath, "r").read()
+        check_palindrome(text)
+    except OSError:
+        print(f"Unable to process file at {filepath}")
+else:
+    print("Invalid choice"
